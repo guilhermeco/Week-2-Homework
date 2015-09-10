3ce6092b4206c6ff8fa1d4fd28a2e0e142ddf13a
Guilherme Coelho <guilhermeismco@gmail.com>
2015-09-10 18:00:10 -0400

    class commit

---
+++ Week 2 Homework/Assets/CollisionListener.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionListener : MonoBehaviour {
+
+	void OnTriggerEnter( Collider otherObject) {
+
+	}
+
+	void OnCollisionEnter( Collision thisCollision) {
+		if (thisCollision.gameObject.name == "Player") {
+		print ("Physics Collision");
+		}
+	}
+}
---
+++ Week 2 Homework/Assets/ForcePusher.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForcePusher : MonoBehaviour {
+
+	private float xForce;
+	private int zTourqe;
+	private GameObject redCube;
+
+	// Use this for initialization
+	void Start () {
+		xForce = 150;
+		zTourqe = -150;
+		redCube = GameObject.Find ("redCube");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.Space) == true) {
+			Rigidbody rb = gameObject.GetComponent<Rigidbody> ();
+			rb.AddForce (new Vector3 (xForce, 0, 0));
+			rb.AddTorque (new Vector3 (150, 0, 0));
+
+		}
+			if (Input.GetKey (KeyCode.Z) == true) {
+				Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+				rb.AddForce (new Vector3 (zTourqe, 0, 0));
+				rb.AddTorque (new Vector3 (150, 0, 0));
+				redCube.transform.localScale = redCube.transform.localScale + new Vector3 (0.1f, 0.1f, 0.1f);
+		}
+	}
+}
